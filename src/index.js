bf58f60017d4cf56a1d88f28ed612189a952e80e
Carlo Gilmar <carlogilmar12@gmail.com>
2020-09-21 23:43:13 -0500

    Building  a simple component

--- src/index.js
+++ src/index.js
@@ -1,13 +1,10 @@
 // This is the entrypoint for the exported module nav
 import nav from "./nav";
-import {top, bottom} from "./footer";
+import {footer} from "./footer";
 import makeButton from "./button"
 import {red, blue, makeColorStyle} from './button-styles'
 
-console.log(nav());
-console.log(top);
-console.log(bottom);
-console.log(makeColorStyle("#FFFFFF"));
-
 const button = makeButton("I'm a Button");
+button.style = makeColorStyle("cyan");
 document.body.appendChild(button);
+document.body.appendChild(footer);
